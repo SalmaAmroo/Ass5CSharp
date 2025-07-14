ea6ce8c4b8a4dd49ed900eede55f9c9cadfa0c34
Admin <Admin@SALMAAMRO>
2025-07-14 21:31:49 +0300

    Add project files.

---
+++ Ass5Csharp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Ass5Csharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
